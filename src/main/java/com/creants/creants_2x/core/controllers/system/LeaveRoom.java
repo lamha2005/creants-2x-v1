98b3a8eecf6d983330bbff401ba1dbeda80deb47
lamhm <hamanhlam2005@gmail.com>
2017-06-26 17:32:36 +0700

    Implement leave room

--- src/main/java/com/creants/creants_2x/core/controllers/system/LeaveRoom.java
+++ src/main/java/com/creants/creants_2x/core/controllers/system/LeaveRoom.java
@@ -1,5 +1,44 @@
 package com.creants.creants_2x.core.controllers.system;
 
-public class LeaveRoom {
+import com.creants.creants_2x.core.controllers.BaseControllerCommand;
+import com.creants.creants_2x.core.controllers.SystemRequest;
+import com.creants.creants_2x.core.entities.Room;
+import com.creants.creants_2x.core.entities.Zone;
+import com.creants.creants_2x.socket.gate.entities.IQAntObject;
+import com.creants.creants_2x.socket.gate.wood.QAntUser;
+import com.creants.creants_2x.socket.io.IRequest;
+
+/**
+ * @author LamHM
+ *
+ */
+public class LeaveRoom extends BaseControllerCommand {
+
+	public static final String KEY_ROOM_ID = "r";
+
+
+	public LeaveRoom(SystemRequest request) {
+		super(SystemRequest.LeaveRoom);
+	}
+
+
+	@Override
+	public boolean validate(IRequest request) throws Exception {
+		return true;
+	}
+
+
+	@Override
+	public void execute(IRequest request) throws Exception {
+		QAntUser user = api.getUserByChannel(request.getSender());
+		Zone zone = user.getZone();
+		IQAntObject reqObj = request.getContent();
+		int roomId = -1;
+		if (reqObj.containsKey(KEY_ROOM_ID)) {
+			roomId = reqObj.getInt(KEY_ROOM_ID);
+		}
+		Room theRoom = (roomId >= 0) ? zone.getRoomById(roomId) : null;
+		api.leaveRoom(user, theRoom, true, true);
+	}
 
 }
